d55cf254eb61bee38a66567780ca25b4f44dda40
IvanPanin675 <jovanni.94.van@gmail.com>
2023-02-01 21:22:41 +0200

    pobeda

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,87 +1,88 @@
-import { Component } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { nanoid } from 'nanoid';
 
 import FormAddContact from './FormAddContact/FormAddContact';
 import FilterSearch from './FilterSearch/FilterSearch';
 import ContactsList from './ContactsList/ContactsList';
 
-export class App extends Component {
-  state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+export function App() {
+  const [contacts, setContacts] = useState([{ id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
       { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
       { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
-    filter: '',
-  };
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },]);
+  const [filter, setFilter] = useState('');
+  const contactsRef = useRef(contacts);
 
-  componentDidMount() {
-    const contacts = JSON.parse(localStorage.getItem('contacts'));
-    if (contacts) {
-      this.setState({ contacts });
+  useEffect(() => {
+    const contactsLocal = JSON.parse(localStorage.getItem('contacts'));
+
+    if (contactsLocal) {
+      setContacts([ ...contactsLocal ]);
+    }  
+  }, [])
+
+  useEffect(() => {
+    if (contactsRef.current === contacts) {
+      return
     }
-  }
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+    contactsRef.current = contacts;
+  },[contacts])
 
-  componentDidUpdate(prevState) {
-    const { contacts } = this.state;
-    if (prevState.contacts !== contacts) {
-      localStorage.setItem('contacts', JSON.stringify(contacts));
-    }
-  }
-
-  onHandleSubmit = data => {
+  const onHandleSubmit = data => {
     const number = data.number;
     const name = data.name;
-    if (this.state.contacts.find(contact => contact.name === name)) {
+
+    if (contacts.find(contact => contact.name === name)) {
       alert(`${name} is already in contacts`);
       return;
     }
-    if (this.state.contacts.find(contact => contact.number === number)) {
+    if (contacts.find(contact => contact.number === number)) {
       alert(`This number:${number} is already in contacts`);
       return;
     }
     const id = nanoid();
     const contact = { id, name, number };
-    this.setState(({ contacts }) => ({ contacts: [...contacts, contact] }));
+    setContacts( [...contacts, contact] );
   };
 
-  onSearchName = e => {
-    this.setState({ filter: e.target.value });
+  const onSearchName = e => {
+    setFilter(e.target.value);
   };
 
-  getVisibleContacts = () => {
-    const { contacts, filter } = this.state;
-    const lowerFilter = filter.toLowerCase();
-    return contacts.filter(contact =>
-      contact.name.toLowerCase().includes(lowerFilter)
-    );
+  const getVisibleContacts = () => {
+    if (contacts) {
+      const lowerFilter = filter.toLowerCase();
+      return contacts.filter(contact =>
+        contact.name.toLowerCase().includes(lowerFilter)
+      );
+    }
   };
 
-  onDeleting = deleteId => {
-    this.setState(prevState => ({
-      contacts: prevState.contacts.filter(contact => contact.id !== deleteId),
-    }));
+  const onDeleting = deleteId => {
+    setContacts(contactsRef => (
+      contactsRef.filter(contact => contact.id !== deleteId)
+    ));
   };
-  render() {
-    const { filter, contacts } = this.state;
+
+
     return (
       <>
         <h1>Phonebook</h1>
-        <FormAddContact onHandleSubmit={this.onHandleSubmit} />
+        <FormAddContact onHandleSubmit={onHandleSubmit} />
         {Boolean(contacts[0]) && (
           <>
             <h2>Contacts</h2>
-            <FilterSearch onSearchName={this.onSearchName} value={filter} />
-            {this.getVisibleContacts().length === 0 || (
+            <FilterSearch onSearchName={onSearchName} value={filter} />
+            {getVisibleContacts().length === 0 || (
               <ContactsList
-                getVisibleContacts={this.getVisibleContacts()}
-                onDelete={this.onDeleting}
+                getVisibleContacts={getVisibleContacts()}
+                onDelete={onDeleting}
               />
             )}
           </>
         )}
       </>
     );
-  }
+  
 }
